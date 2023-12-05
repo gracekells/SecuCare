5e2eb731fb3fddcd02aa7281c8624b5f45563f58
Grace Kelly <graceksd16@gmail.com>
2023-12-05 15:47:02 +0700

    update

---
+++ app/src/main/java/com/dicoding/secucare/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.secucare.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.secucare.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
